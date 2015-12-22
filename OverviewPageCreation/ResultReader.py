f65d068f67d7b223e39cef9a9acba0d72994db43
Felix Bachmair <bachmair@phys.ethz.ch>
2015-12-22 21:29:07 +0000

    exception handling to find out whats the error
    git-svn-id: svn+ssh://utka42.ethz.ch/rd42/trunk@1447 5e05d84c-b09a-4460-8136-89425579e6ea

--- OverviewPageCreation/ResultReader.py
+++ OverviewPageCreation/ResultReader.py
@@ -75,7 +75,18 @@ class ResultReader:
         if newRunNo in self.map:
             try:
                 config.set('RunInfo', 'repeatercardno', '%s' % self.map[newRunNo]['repeaterCard'])
+            except:
+                config.set('RunInfo', 'repeatercardno','unknown')
+            try:
                 config.set('RunInfo', 'voltage', '%+4d' % self.map[newRunNo]['biasVoltage'])
+            except:
+                voltage = None
+                while not type(voltage) == int:
+                    print 'Cannot Set Voltage for Run %d. Please enter by hand in Volt.'%newRunNo
+                    print 'possibilities are ',self.map[newRunNo]['biasVoltage']
+                    voltage = raw_input()
+                config.set('RunInfo', 'voltage','%+4d'%voltage)
+            try:
                 config.set('RunInfo', 'currentbegin', '%s' % (self.map[newRunNo]['currentBegin']))
                 config.set('RunInfo', 'currentend', '%s' % (self.map[newRunNo]['currentEnd']))
             except Exception as e:
