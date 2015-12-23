102be99670a93d99a436ffba7cffff2cde4f17dc
Felix Bachmair <bachmair@phys.ethz.ch>
2015-12-23 10:03:46 +0000

    working on easier asymmetric eta correction method
    git-svn-id: svn+ssh://utka42.ethz.ch/rd42/trunk@1449 5e05d84c-b09a-4460-8136-89425579e6ea

--- createAsymmetricEtaSample.C
+++ createAsymmetricEtaSample.C
@@ -23,13 +23,6 @@ UInt_t eventNumber;
 UInt_t runNumber;
 Float_t chargeShareDia;
 Float_t chargeShareSil;
-Float_t diaPedestalMeanCMN[128];
-Float_t diaPedestalSigmaCMN[128];
-Float_t diaPedestalMean[128];
-Float_t diaPedestalMeanOld[128];
-Float_t diaPedestalSigma[128];
-Float_t PedestalMean[8][256];
-Float_t PedestalMeanOld[8][256];
 UChar_t Det_ADC[8][256];
 UShort_t Dia_ADC[128];
 int verbosity = 5;
@@ -101,44 +94,11 @@ void setBranchAdresses(TTree* tree){
 		tree->SetBranchAddress("D3Y_ADC",&Det_ADC[7]);
 		if(verbosity>3)cout<<"Set Branch \"D3Y_ADC\""<<endl;
 	}
-	//tree->SetBranchAddress("Dia_ADC",&Dia_ADC);
 	if(tree->FindBranch("DiaADC")){
 		tree->SetBranchAddress("DiaADC",&Dia_ADC);
 		if(verbosity>3)cout<<"Set Branch \"DiaADC\""<<endl;
 	}
 
-	//		if(tree->FindBranch("Dia_PedMean")){
-	//			tree->SetBranchAddress("Dia_PedMean",&PedestalMean[8]);
-	//			if(verbosity>3)cout<<"Set Branch \"Dia_PedMean\""<<endl;
-	//		}
-	//		if(tree->FindBranch("Dia_PedWidth")){
-	//			tree->SetBranchAddress("Dia_PedWidth",&Det_PedWidth[8]);
-	//			if(verbosity>3)cout<<"Set Branch \"Dia_PedWidth\""<<endl;
-	//		}
-	if(tree->FindBranch("PedestalMean")){
-		tree->SetBranchAddress("PedestalMean",&PedestalMean);
-		if(verbosity>3)cout<<"Set Branch \"PedestalMean\""<<endl;
-	}
-	//		if(tree->FindBranch("PedestalSigma")){
-	//			tree->SetBranchAddress("PedestalSigma",&pedestalSigma);
-	//			if(verbosity>3)cout<<"Set Branch \"PedestalSigma\""<<endl;
-	//		}
-	if(tree->FindBranch("diaPedestalMean")){
-		tree->SetBranchAddress("diaPedestalMean",&diaPedestalMean);
-		if(verbosity>3)cout<<"Set Branch \"diaPedestalMean\""<<endl;
-	}
-	if(tree->FindBranch("diaPedestalSigma")){
-		tree->SetBranchAddress("diaPedestalSigma",&diaPedestalSigma);
-		if(verbosity>3)cout<<"Set Branch \"diaPedestalSigma\""<<endl;
-	}
-	if(tree->FindBranch("diaPedestalMeanCMN")){
-		tree->SetBranchAddress("diaPedestalMeanCMN",&diaPedestalMeanCMN);
-		if(verbosity>3)cout<<"Set Branch \"diaPedestalMeanCMN\""<<endl;
-	}
-	if(tree->FindBranch("diaPedestalSigmaCMN")){
-		tree->SetBranchAddress("diaPedestalSigmaCMN",&diaPedestalSigmaCMN);
-		if(verbosity>3)cout<<"Set Branch \"diaPedestalSigmaCMN\""<<endl;
-	}
 }
 void showStatusBar(int nEvent,int nEvents,int updateIntervall,bool show,bool makeNewLine){
 	if(nEvent+1>=nEvents)nEvent++;
@@ -154,70 +114,30 @@ void showStatusBar(int nEvent,int nEvents,int updateIntervall,bool show,bool mak
 	}
 	if(makeNewLine&&nEvent+1>=nEvents)cout<<endl;
 }
-
 void updateSilicon(){
 	for (UInt_t det = 0; det < 8; det++){
-		Float_t share = 0;
 		UInt_t startChannel = 0;
 		UInt_t endChannel = 127;
 		if(det == 2 || det == 6){
 			startChannel = 127;
 			endChannel = 0;
-//			cout<<det<< " startchannel: "<<startChannel<<" - " << endChannel<<endl;
 		}
 		bool finished = false;
+		Float_t alpha = chargeShareSil;
+		UShort_t adc = 0;
 		for(UInt_t ch=startChannel;!finished;){
-			if(det==2||det==6){
-				if(eventNumber <5&&false)
-					cout<<det<<" "<<eventNumber<<" ch: "<<ch<<endl;
-			}
-			UShort_t oldADC = Det_ADC[det][ch];
-			Float_t adc = oldADC;
-			Float_t pedMean = PedestalMean[det][ch];
-			if (pedMean<37){
-                UInt_t key = det*1000+ch;
-                bool bprint = false;
-                if (errorCounter.count(key)){
-                    if (errorCounter[key] < 10){
-                        errorCounter[key]++;
-                        bprint = true;
-                    }
-                }
-                else{
-                    bprint = true;
-                    errorCounter[key]=1;
-                }
-                if (bprint){
-                        cout<<"\ntaking old pedValue for: \t";
-                        cout<<eventNumber<<" "<<ch<<" "<<(int)	adc<<", "<<pedMean<<"->";
-                }
-                pedMean = PedestalMeanOld[det][ch];
-                if (bprint){
-                    cout<<pedMean<<" = "<<", "<< share<<" --> ";
-                }
-			}
-			else PedestalMeanOld[det][ch] = pedMean;
-			Float_t signal = (Float_t)adc-pedMean;
-			//if(share>10)
-			if(eventNumber >15000 &&eventNumber<16000&&signal>10&&false)
-				cout<<eventNumber<<" "<<det<<" "<<ch<<" "<<adc<<", "<<pedMean<<" = "<<signal<<", "<< share<<" --> ";
-			adc += share;
-			UShort_t newADC = UShort_t(adc+.5);
-			if(adc>255)
+			UShort_t measured_adc = Det_ADC[det][ch];
+			Float_t real_adc = ((Float_t)measured_adc-(Float_t)adc*alpha)/(1-alpha);
+			UShort_t newADC;
+			adc = newADC;
+			if(newADC>255)
 				newADC = 255;
-			if(eventNumber>15000 && eventNumber < 16000&&signal>10&&false)
-				cout<<newADC<<" "<<newADC-oldADC<<endl;
-			Det_ADC[det][ch] = newADC;
-			signal = (Float_t)(adc-pedMean);
-			share = signal * chargeShareSil;
-			if(det==2||det==6)
-				finished = (ch==endChannel);
 			else
-				finished = (ch==endChannel);
-			if(det==2||det == 6 ){
+			    newADC  = UShort_t(real_adc+.5);
+			Det_ADC[det][ch] = newADC;
+            finished = (ch==endChannel);
+			if(det==2||det == 6 )
 				ch--;
-//				share*=-1;
-			}
 			else
 				ch++;
 		}
@@ -228,40 +148,24 @@ void updateDiamond(){
 	Float_t share =0;
 	for(UInt_t ch=0;ch<128;ch++){
 		Float_t adc = Dia_ADC[ch];
-		Float_t pedMean = diaPedestalMean[ch];
-		if (pedMean<500){
-//			cout<<"\ntaking old pedValue for: \t";
-//			cout<<eventNumber<<" "<<ch<<" "<<adc<<", "<<pedMean<<" = "<<", "<< share<<" --> "<<endl;
-			pedMean = diaPedestalMeanOld[ch];
-		}
-		else diaPedestalMeanOld[ch] = pedMean;
-		Float_t signal = (Float_t)adc-pedMean;
-		//if(share>10)
-		if(eventNumber >15637 &&eventNumber<15640&&false)
-			cout<<eventNumber<<" dia "<<ch<<" "<<adc<<", "<<pedMean<<" = "<<signal<<", "<< share<<" --> ";
-		adc += share;
-		UShort_t newADC = UShort_t(adc+.5);
-		if(newADC>4095)
-			newADC = 4095;
-		if(eventNumber>15637 && eventNumber < 15640&&false)
-			cout<<newADC<<endl;
-		Dia_ADC[ch] = newADC;
-		signal = (Float_t)newADC-pedMean;
-		share = signal * chargeShareDia;
-	}
-	if(eventNumber>15637 && eventNumber < 15640&&false){
-		char t;
-		cin>>t;
-	}
-
+        Float_t alpha = chargeShareSil;
+        UShort_t adc = 0;
+        for(UInt_t ch=startChannel;!finished;){
+            UShort_t measured_adc = Det_ADC[det][ch];
+            Float_t real_adc = ((Float_t)measured_adc-(Float_t)adc*alpha)/(1-alpha);
+            UShort_t newADC;
+            adc = newADC;
+            if(newADC>255)
+                newADC = 255;
+            else
+                newADC  = UShort_t(real_adc+.5);
+            Dia_ADC[ch] = newADC;
+            finished = (ch==endChannel);
+            ch++;
+        }
 }
 
 void LoopOverTree(TTree* inputTree,TTree* outputTree){
-	for(int i=0;i<128;i++)
-		diaPedestalMeanOld[i]=0;
-	for (int det=0;det<8;det++)
-		for(int ch=0;ch<256;ch++)
-			PedestalMeanOld[det][ch]=0;
 	setBranchAdresses(inputTree);
 	UInt_t nEntries = inputTree->GetEntries();
 //	char t; cin>>t;
@@ -301,7 +205,7 @@ int createAsymmetricEtaSample(int runNo,float silCor,float diaCor,int corRunNo= 
 
 
 
-	TString fileName = TString::Format("pedestalData.%05d.root",runNumber);
+	TString fileName = TString::Format("rawData.%05d.root",runNumber);
 	cout<<"Reading file '"<<fileName<<"'"<<endl;
 
 	TFile * file = (TFile*)TFile::Open(fileName);
@@ -310,7 +214,7 @@ int createAsymmetricEtaSample(int runNo,float silCor,float diaCor,int corRunNo= 
 		return -1;
 	}
 	TTree* tree;
-	file->GetObject("pedestalTree",tree);
+	file->GetObject("rawTree",tree);
 
 	if(tree==NULL) {
 		tree=(TTree*)getTreeName(file);
@@ -345,8 +249,6 @@ int createAsymmetricEtaSample(int runNo,float silCor,float diaCor,int corRunNo= 
     else
         cout<<"Link already exists: "<<linkFileName<<endl;
 	return 1;
-
-
 }
 
 
